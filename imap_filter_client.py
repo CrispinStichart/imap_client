a30d31cd6ae4d4cd0a106598f4d0c3a7e156a0a2
Crispin A. Stichart <crispinstichart@gmail.com>
2022-02-13 13:00:29 -0600

    Put everything in a class

--- imap_filter_client.py
+++ imap_filter_client.py
@@ -1,3 +1,4 @@
+import argparse
 import configparser
 import email
 import importlib.util
@@ -35,195 +36,217 @@ class Envelope:
         self.subject = envelope.subject.decode()
 
 
-def load_filter_modules() -> dict:
-    p = Path(os.path.realpath(__file__))
+class ImapFilterClient:
+    def __init__(self, command_line_args=None):
+        self.config = self.load_config(command_line_args)
 
-    filter_modules = {}
+        modules = self.load_filter_modules()
+        self.filters = self.get_filter_classes_from_modules(modules)
 
-    # Not sure how kosher this all is, I've never done any dynamic
-    # loading of modules before.
-    for file in sorted(p.parent.glob("filters/*.py")):
-        if file.name not in ["__init__.py", "mail_filter.py"]:
-            log.debug(f"loading module from: {file}")
-            spec = importlib.util.spec_from_file_location("filters." + file.name, file)
-            module = importlib.util.module_from_spec(spec)
-            sys.modules[file.name] = module
-            filter_modules[file.name] = module
-            spec.loader.exec_module(module)
+        self.last_seen_uid = -1
 
-    return filter_modules
+    def load_config(self, command_line_args):
+        config_file = configparser.ConfigParser()
+        config_file.read("imap_filter.conf")
 
+        conf = {
+            "host": config_file.get("DEFAULT", "host", fallback=""),
+            "username": config_file.get("DEFAULT", "username", fallback=""),
+            "password": config_file.get("DEFAULT", "password", fallback=""),
+        }
 
-def get_filter_classes_from_modules(filter_modules) -> dict[str, mail_filter.Filter]:
-    filter_classes: dict[str, mail_filter.Filter] = {}
+        return conf
 
-    # We go through the modules that we imported and find the class that
-    # subclasses our abstract mail_filter.Filter class, then we
-    # instantiate it and save it with the module name.
-    for module_name, module in filter_modules.items():
-        for name, obj in inspect.getmembers(module):
-            if inspect.isclass(obj) and issubclass(obj, mail_filter.Filter):
-                filter_classes[module_name] = obj()
+    def load_filter_modules(self) -> dict:
+        p = Path(os.path.realpath(__file__))
 
-    return filter_classes
+        filter_modules = {}
 
+        # Not sure how kosher this all is, I've never done any dynamic
+        # loading of modules before.
+        for file in sorted(p.parent.glob("filters/*.py")):
+            if file.name not in ["__init__.py", "mail_filter.py"]:
+                log.debug(f"loading module from: {file}")
+                spec = importlib.util.spec_from_file_location(
+                    "filters." + file.name, file
+                )
+                module = importlib.util.module_from_spec(spec)
+                sys.modules[file.name] = module
+                filter_modules[file.name] = module
+                spec.loader.exec_module(module)
 
-def fetch_email(msg_id: int) -> EmailMessage:
-    # We have to establish a new connection because we put the other
-    # connection into idle mode, which causes the server to ignore
-    # further commands. We establish a new connection for every new
-    # message, since in a low-traffic inbox, it may be hours or more
-    # between messages, and I'm not sure how IMAPClient/imaplib handles
-    # long-lived connections.
-    with establish_connection() as client:
-        log.debug(f"Fetching message with ID={msg_id}")
-        for msg_id, data in client.fetch([msg_id], ["ENVELOPE", "RFC822"]).items():
-            # message_from_bytes is typhinted (in the typeshed stubs) as
-            # returning Message, but that's only when policy=compat32
-            email_message = cast(
-                EmailMessage,
-                email.message_from_bytes(data[b"RFC822"], policy=policy.default),
-            )
+        return filter_modules
 
-            # Monkey patch the envelope into the message. This is
-            # probably not the best way to handle this.
-            email_message.envelope = Envelope(data[b"ENVELOPE"])
+    def get_filter_classes_from_modules(
+        self,
+        filter_modules,
+    ) -> dict[str, mail_filter.Filter]:
 
-            # Monkey patch the msg_id into the message. Again, probably
-            # not the best way to handle this.
-            email_message.id = msg_id
+        filter_classes: dict[str, mail_filter.Filter] = {}
 
-    return email_message
+        # We go through the modules that we imported and find the class that
+        # subclasses our abstract mail_filter.Filter class, then we
+        # instantiate it and save it with the module name.
+        for module_name, module in filter_modules.items():
+            for name, obj in inspect.getmembers(module):
+                if inspect.isclass(obj) and issubclass(obj, mail_filter.Filter):
+                    filter_classes[module_name] = obj()
 
+        return filter_classes
 
-def filter_thread(download_q: Queue, shutdown_event: threading.Event):
-    filter_modules = load_filter_modules()
-    filter_classes = get_filter_classes_from_modules(filter_modules)
+    def fetch_email(self, msg_id: int) -> EmailMessage:
+        # We have to establish a new connection because we put the other
+        # connection into idle mode, which causes the server to ignore
+        # further commands. We establish a new connection for every new
+        # message, since in a low-traffic inbox, it may be hours or more
+        # between messages, and I'm not sure how IMAPClient/imaplib handles
+        # long-lived connections.
+        with self.establish_connection() as client:
+            log.debug(f"Fetching message with ID={msg_id}")
+            for msg_id, data in client.fetch([msg_id], ["ENVELOPE", "RFC822"]).items():
+                # message_from_bytes is typhinted (in the typeshed stubs) as
+                # returning Message, but that's only when policy=compat32
+                email_message = cast(
+                    EmailMessage,
+                    email.message_from_bytes(data[b"RFC822"], policy=policy.default),
+                )
 
-    while True:
-        # We check the queue every couple of second, and spend the rest
-        # of the time waiting on a shutdown event. so we can join this
-        # thread.
-        try:
-            uid: int = download_q.get_nowait()
-            msg = fetch_email(uid)
+                # Monkey patch the envelope into the message. This is
+                # probably not the best way to handle this.
+                email_message.envelope = Envelope(data[b"ENVELOPE"])
 
-            for name, filter_c in filter_classes.items():
-                log.debug(f"Sending message {uid} to {name} ")
-                processed = filter_c.filter(msg)
-                log.debug(f"Processed: {processed}")
-                # filters will return true if they deleted or moved
-                # the message, such that any other filters may have
-                # undefined behavior.
-                if processed:
-                    break
-        except queue.Empty:
-            if shutdown_event.wait(2.0):
-                break
+                # Monkey patch the msg_id into the message. Again, probably
+                # not the best way to handle this.
+                email_message.id = msg_id
 
+        return email_message
 
-@contextmanager
-def establish_connection():
-    client = None
-    try:
-        config = configparser.ConfigParser()
-        config.read("imap_filter.conf")
-
-        imap_host = config["DEFAULT"]["host"]
-        username = config["DEFAULT"]["username"]
-        password = config["DEFAULT"]["password"]
-
-        client = IMAPClient(host=imap_host)
-        client.login(username, password)
-        client.enable("UTF8=ACCEPT")
-        client.select_folder("INBOX")
-
-        yield client
-    finally:
-        if client:
-            client.logout()
-
-
-def get_last_checked_uid(client: imapclient.IMAPClient, catchup=False):
-    if catchup:
-        try:
-            with open(LAST_SEEN_FILENAME, "r") as f:
-                uid = int(f.readline().strip())
-                log.debug(f"Read UID from {LAST_SEEN_FILENAME}: {str(uid)}")
-                return uid
-        except FileNotFoundError:
-            catchup = False
-
-    if not catchup:
-        log.debug("Searching for most recent email")
-        # The "*" stands for the highest numbered (most recent) UID
-        uid = client.search(["UID", "*"])[0]
-        with open(LAST_SEEN_FILENAME, "w") as f:
-            f.write(str(uid))
-        return uid
-
-
-def main():
-    log.setLevel(logging.DEBUG)
-    with establish_connection() as client:
-        client: imapclient.IMAPClient
-
-        last_checked_uid = get_last_checked_uid(client)
-
-        download_q: Queue = Queue()
-
-        shutdown_event = threading.Event()
-        fetcher_thread = threading.Thread(
-            target=filter_thread, args=(download_q, shutdown_event)
-        )
-        fetcher_thread.start()
-
-        # TODO:
-
+    def filter_thread(self, download_q: Queue, shutdown_event: threading.Event):
         while True:
+            # We check the queue every couple of second, and spend the rest
+            # of the time waiting on a shutdown event, so we can join this
+            # thread.
             try:
-                log.debug(f"Searching for messages above UID {last_checked_uid}")
-                # Note: UID ranges are inclusive. "*" is the hightest UID in the inbox.
-                results = client.search(f"UID {str(last_checked_uid + 1)}:*")
-                for uid in results:
-                    log.debug(f"Putting UID={uid} into download queue")
-                    download_q.put(uid)
+                uid: int = download_q.get_nowait()
+                msg = self.fetch_email(uid)
 
-                # From what I can tell reading the docs, the search result
-                # isn't guaranteed to return the UIDs in order.
-                last_checked_uid = max(results)
+                for name, filter_c in self.filters.items():
+                    log.debug(f"Sending message {uid} to {name} ")
+                    processed = filter_c.filter(msg)
+                    log.debug(f"Processed: {processed}")
+                    # filters will return true if they deleted or moved
+                    # the message, such that any other filters may have
+                    # undefined behavior.
+                    if processed:
+                        break
+            except queue.Empty:
+                if shutdown_event.wait(2.0):
+                    break
 
-                # Save the new last_seen
-                with open(LAST_SEEN_FILENAME, "w") as f:
-                    f.write(str(last_checked_uid))
+    @contextmanager
+    def establish_connection(self, folder="INBOX"):
+        # Instead of using IMAPClient's built in context manager, we use
+        # our own so we can login and set some defaults without repitition.
 
-                # Some notes: 1) when IDLEing, the IMAP server does not
-                # return UIDs for emails, it returns the message ID,
-                # which don't persist between sessions and may be
-                # reassigned by certain operations. So whenver there's
-                # activity, we poll the server for messages based on the
-                # date of the most recently seen email. 2) Since idle
-                # mode prevents the server from responding to any other
-                # commands, we have to drop out of idle mode before we
-                # can poll the server. 3) There's a timeout on the IDLE
-                # because the docs recomend it, because the server will
-                # close long-lived socket connections, and IMAPClient
-                # doesn't do anything to keep the connection alive.
-                client.idle()
-                log.info("Client is now in IDLE mode, waiting for response...")
-                log.debug("Waiting for IDLE response...")
-                response = client.idle_check(60 * 5)
-                log.debug(f"Got IDLE response: {response}")
-                log.debug("Exiting IDLE mode")
-                client.idle_done()
-            except KeyboardInterrupt:
-                log.debug("Got keyboard inturupt")
-                shutdown_event.set()
-                break
+        client = None
+        try:
+            client = IMAPClient(host=self.config["host"])
+            client.login(self.config["username"], self.config["password"])
+            client.enable("UTF8=ACCEPT")
+            client.select_folder(folder)
 
-        log.debug("Waiting for fetch thread to join")
-        fetcher_thread.join()
+            yield client
+        finally:
+            if client:
+                client.logout()
+
+    def get_last_checked_uid(self, client: imapclient.IMAPClient, catchup=False):
+        if catchup:
+            try:
+                with open(LAST_SEEN_FILENAME, "r") as f:
+                    uid = int(f.readline().strip())
+                    log.debug(f"Read UID from {LAST_SEEN_FILENAME}: {str(uid)}")
+                    self.last_seen_uid = uid
+                    return uid
+            except FileNotFoundError:
+                catchup = False
+
+        if not catchup:
+            log.debug("Searching for most recent email")
+            # The "*" stands for the highest numbered (most recent) UID
+            uid = client.search(["UID", "*"])[0]
+            with open(LAST_SEEN_FILENAME, "w") as f:
+                f.write(str(uid))
+            self.last_seen_uid = uid
+            return uid
+
+    def main(self):
+        log.setLevel(logging.DEBUG)
+        with self.establish_connection() as client:
+            client: imapclient.IMAPClient
+
+            self.get_last_checked_uid(client)
+            assert self.last_seen_uid != -1
+
+            download_q: Queue[int] = Queue()
+
+            shutdown_event = threading.Event()
+            fetcher_thread = threading.Thread(
+                target=self.filter_thread, args=(download_q, shutdown_event)
+            )
+            fetcher_thread.start()
+
+            # TODO:
+
+            while True:
+                try:
+                    log.debug(f"Searching for messages above UID {self.last_seen_uid}")
+                    # Note: UID ranges are inclusive. "*" is the
+                    # highest UID in the inbox. From what I can tell
+                    # reading the docs, the search result isn't
+                    # guaranteed to return the UIDs in order, hence the
+                    # .sort().
+                    results = sorted(client.search(f"UID {str(self.last_seen_uid)}:*"))
+                    # We slice the result to get rid of the one we've already seen
+                    for uid in results[1:]:
+                        log.debug(f"Putting UID={uid} into download queue")
+                        download_q.put(uid)
+
+                    self.last_seen_uid = results[-1]
+
+                    # Save the new last_seen
+                    with open(LAST_SEEN_FILENAME, "w") as f:
+                        f.write(str(self.last_seen_uid))
+
+                    # Some notes: 1) when IDLEing, the IMAP server does not
+                    # return UIDs for emails, it returns the message ID,
+                    # which don't persist between sessions and may be
+                    # reassigned by certain operations. So whenver there's
+                    # activity, we poll the server for messages based on the
+                    # date of the most recently seen email. 2) Since idle
+                    # mode prevents the server from responding to any other
+                    # commands, we have to drop out of idle mode before we
+                    # can poll the server. 3) There's a timeout on the IDLE
+                    # because the docs recomend it, because the server will
+                    # close long-lived socket connections, and IMAPClient
+                    # doesn't do anything to keep the connection alive.
+                    client.idle()
+                    log.info("Client is now in IDLE mode, waiting for response...")
+                    log.debug("Waiting for IDLE response...")
+                    response = client.idle_check(60 * 5)
+                    log.debug(f"Got IDLE response: {response}")
+                    log.debug("Exiting IDLE mode")
+                    client.idle_done()
+                except KeyboardInterrupt:
+                    log.debug("Got keyboard inturupt")
+                    shutdown_event.set()
+                    break
+
+            log.debug("Waiting for fetch thread to join")
+            fetcher_thread.join()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    filter_client = ImapFilterClient()
+    filter_client.main()
